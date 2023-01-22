718e1ac83e72236099fff0f22a158cf42ad6a780
ryanyae <ryancyae@gmail.com>
2023-01-21 22:16:34 -0800

    fixed!

--- front-end/src/pages/UploadImage.js
+++ front-end/src/pages/UploadImage.js
@@ -10,30 +10,30 @@ export default function UploadImage() {
 
     return (
         <>
-        <div id="mainDiv">
+        <div id="mainDiv2">
 
             
          
 
             <form>
-                <div class="nameGroup">
-                    <label for="name">Name:</label>
+                <div className="nameGroup">
+                    <label htmlFor="name">Name:</label>
                     <input type="text" placeholder="Name" id="clothingName"></input>
                 </div>
 
-                <div class="typeGroup">
-                    <label for="type">Type:</label>
+                <div className="typeGroup">
+                    <label htmlFor="type">Type:</label>
                     <input type="text" placeholder="Type: Top, Bottom, Shoes" id="clothingType"></input>
 
                 </div>
 
-                <div class="colorGroup">
-                    <label for="color">Color:</label>
+                <div className="colorGroup">
+                    <label htmlFor="color">Color:</label>
                     <input type="text" placeholder="Color" id="clothingColor"></input>
                 </div>
 
-                <div class="fileGroup">
-                    <label for="imageFile">Upload image:</label>
+                <div className="fileGroup">
+                    <label htmlFor="imageFile">Upload image:</label>
                     <input type="file" id="clothingFile" name="imageFile"></input>
 
                 </div>
