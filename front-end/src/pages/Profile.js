80c1c65362c1c211da4300151e6a974bf7364433
alim1801 <Annabellim0118@gmail.com>
2023-01-21 23:46:25 -0800

    added pop up

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -3,10 +3,23 @@ import Header from "./Header"
 import MasterList from "./MasterList"
 import Item from "./Item"
 import Wearing from "./Wearing"
+import UploadImage from "./UploadImage"
+import {useRef} from 'react'
+
 
 const Profile = () => {
+    const addPopup = useRef();
 
-
+    const handlePopUp = () => {
+        if (addPopup.current.style.display === "block") {
+            addPopup.current.style.display="none"
+        } else {
+            addPopup.current.style.display="block"
+        }
+            
+        
+        
+    }
 
     return (
         <>
@@ -16,8 +29,38 @@ const Profile = () => {
                     <div id="masterListDiv">
                         <MasterList />
                     </div>
-                    <div id="addingButtonDiv">
-                        {/* Button to add new clothing */}
+                    <div id="addingButtonDiv" onClick={() => {
+                        handlePopUp();
+                    }}>
+                <div>Add Clothing</div>
+                    <div ref={addPopup} id="mainDiv2">
+
+                        <form>
+                            <div className="nameGroup">
+                                <label htmlFor="name">Name:</label>
+                                <input type="text" placeholder="Name" id="clothingName"></input>
+                            </div>
+
+                            <div className="typeGroup">
+                                <label htmlFor="type">Type:</label>
+                                <input type="text" placeholder="Type: Top, Bottom, Shoes" id="clothingType"></input>
+
+                            </div>
+
+                            <div className="colorGroup">
+                                <label htmlFor="color">Color:</label>
+                                <input type="text" placeholder="Color" id="clothingColor"></input>
+                            </div>
+
+                            <div className="fileGroup">
+                                <label htmlFor="imageFile">Upload image:</label>
+                                <input type="file" id="clothingFile" name="imageFile"></input>
+
+                            </div>
+                    
+                        </form>
+
+                    </div>
                     </div>
                 </div>
                 <div id="middle" className="column">
@@ -27,6 +70,9 @@ const Profile = () => {
                     <Wearing />
                 </div>
             </div>
+
+
+            
         </>
     )
 }
